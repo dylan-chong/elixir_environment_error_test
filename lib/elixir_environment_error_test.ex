abcd160d8d3d5cf4bdc6d2de2b5bc3b78c490d05
Dylan Chong <dylan.chong@icloud.com>
2018-01-06 11:24:25 +1300

    Addd TestMod

--- lib/elixir_environment_error_test.ex
+++ lib/elixir_environment_error_test.ex
@@ -16,3 +16,14 @@ defmodule ElixirEnvironmentErrorTest do
     :world
   end
 end
+
+defmodule TestMod do
+  def call do
+    accept(9)
+    accept(-3)
+  end
+  defp accept(number) when abs(number) >= 8 do # Guard test 0 | 1 | 2 | 3 >= 8 can never succeed
+    :first
+  end
+  defp accept(number), do: :second
+end
